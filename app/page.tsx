52e24d0c01ca6d335d07b150f1d2f1c7be07ee56
pivnati <natanmuleta911@gmail.com>
2025-08-21 06:48:48 -1200

    create the companionslist components

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,18 @@
-import { Button } from "@/components/ui/button";
 import React from "react";
+import CompanionCard from '../components/CompanionCard';
 
 const Page = () => {
   return (
-    <div>
-      <h1 className="text-2xl underline">Welcome to my Saas App</h1>
-      <Button>
-        Let get started
-      </Button>
-    </div>
+    <main>
+      <h1 className="text-2xl underline">Popular Companion</h1>
+        <section className="home-sections">
+
+            <CompanionCard/>
+            <CompanionCard/>
+            <CompanionCard/>
+
+        </section>
+    </main>
   );
 };
 
