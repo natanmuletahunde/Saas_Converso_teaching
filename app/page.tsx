bfadd351c28623e9035e04d40a77299dad6ed14b
pivnati <natanmuleta911@gmail.com>
2025-08-21 15:24:26 -1200

    finish the companioncard

--- app/page.tsx
+++ app/page.tsx
@@ -2,16 +2,38 @@ import React from "react";
 import CompanionCard from "../components/CompanionCard";
 import CompanionsList from "@/components/CompanionsList";
 import CTA from "@/components/CTA";
+import { subjects } from '../constants/index';
 
 const Page = () => {
   return (
     <main>
       <h1>Popular Companion</h1>
-      <section className="home-sections">
-        <CompanionCard />
-        <CompanionCard />
-        <CompanionCard />
-      </section>
+      <section className="home-section">
+         <CompanionCard
+        id='123'
+        name='Neural the Brainy Explorer'
+        topic="Nueral Networks of the Brain"
+        subject='Science'
+        duration={45}
+        color='#ffda6e'
+        />
+            <CompanionCard
+        id='456'
+        name='Countsy the Number Wizard'
+        topic="Derivative & Integral"
+        subject='maths'
+        duration={30}
+        color='#e5d0ff'
+        />
+          <CompanionCard
+        id='789'
+        name='Verba the Vocabulary Builder'
+        topic="Languages"
+        subject='English Literature'
+        duration={30}
+        color='#BDE7FF'
+        />
+          </section>
       <section className="home-section">
         <CompanionsList />
         <CTA />
