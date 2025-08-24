71e47647b6b9fafad118cec2fd8f70007d698361
pivnati <natanmuleta911@gmail.com>
2025-08-24 07:18:04 -1200

    finish the companion form to redirect to the companion session

--- components/CompanionForm.tsx
+++ components/CompanionForm.tsx
@@ -25,6 +25,8 @@ import { subjects } from "@/constants";
 import CompanionCard from "./CompanionCard";
 import { Textarea } from "./ui/textarea";
 import { voices } from "../constants/index";
+import { createCompanion } from "@/lib/actions/companion.actions";
+import { redirect } from "next/navigation";
 const formSchema = z.object({
   name: z.string().min(1, { message: "Companion is required." }),
   subject: z.string().min(1, { message: "Subject is required." }),
@@ -47,9 +49,17 @@ const CompanionForm = () => {
     },
   });
 
-  const onSubmit = (values: z.infer<typeof formSchema>) => {
-    console.log(values);
-  };
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+  const companion = await createCompanion(values)
+  if(companion){
+    redirect(`/companions/${companion.id}`);
+  }
+  else
+  {
+    console.log("Failed to create companion");
+  }
+  redirect('/')
+};
 
   return (
     <Form {...form}>
