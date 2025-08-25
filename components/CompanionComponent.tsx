019a25479a90e9880cecbf174c5802b7919c819e
pivnati <natanmuleta911@gmail.com>
2025-08-24 20:39:36 -1200

    destructure the companion data from the id

--- components/CompanionComponent.tsx
+++ components/CompanionComponent.tsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 const CompanionComponent = () => {
   return (
-    <section>
+    <section className='flex flex-col h-[70vh]'>
+        <section className='flex gap-8 max-sm:flex-col'>
+            <div className='companion-section'>
+                <div></div>
+            </div>
+        </section>
        
     </section>
   )
