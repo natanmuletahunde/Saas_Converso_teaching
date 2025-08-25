f6aa7a8a1721fcf4fc3ee51558ec8b59018f1d91
pivnati <natanmuleta911@gmail.com>
2025-08-24 12:53:01 -1200

    make the companion to get from the supabase

--- app/companions/page.tsx
+++ app/companions/page.tsx
@@ -1,8 +1,23 @@
-const CompanionsLibrary = () => {
+import CompanionCard from '@/components/CompanionCard';
+import { getAllCompanions } from '@/lib/actions/companion.actions';
+const CompanionsLibrary = async({ searchParams}:SearchParams) => {
+const filters = await searchParams;
+const subject = filters.subject ? filters.subject:'';
+const topic = filters.topic ? filters.topic:'';
+const Companions = await getAllCompanions({subject, topic});
+console.log(Companions)
   return (
-    <div>
-          Companions Library
-    </div>
+    <main>
+        <section className='flex justify-between gap-4 max-sm:flex-col'>
+  <h1> Companion Library</h1>
+  <div className='flex gap-4'>Filters</div>
+        </section>
+        <section className='companion-grid'>
+            {Companions.map((companion)=>(
+                <CompanionCard key={companion.id} {...companion}/>
+            ))}
+        </section>
+    </main>
   )
 }
 
