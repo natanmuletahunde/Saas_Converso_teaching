19b3422ae9cfc52a170a3ead1cbab34a4d957708
pivnati <natanmuleta911@gmail.com>
2025-08-25 00:32:15 -1200

    install and include the sentry in the folder project

---
+++ sentry.server.config.ts
@@ -0,0 +1,18 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://f552c8e7f2b6f861cddedb6921114e75@o4509904689561600.ingest.us.sentry.io/4509904716431360",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Enable logs to be sent to Sentry
+  enableLogs: true,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
