70ebb3993974c6ec02438a508d30ecdf7e8547bf
pivnati <natanmuleta911@gmail.com>
2025-08-24 21:24:48 -1200

    install the lottie-react

--- components/CompanionComponent.tsx
+++ components/CompanionComponent.tsx
@@ -1,16 +1,41 @@
-import React from 'react'
-import { getSubjectColor } from '@/lib/utils'
-const CompanionComponent = ({companionId , subject , topic,name,userName,userImage,style,voice }:CompanionComponentProps) => {
-  return (
-    <section className='flex flex-col h-[70vh]'>
-        <section className='flex gap-8 max-sm:flex-col'>
-            <div className='companion-section'>
-                <div className='companion-avatar' style={{backgroundColor:getSubjectColor(subject)}} ></div>
-            </div>
-        </section>
-       
-    </section>
-  )
-}
+'use client';
 
-export default CompanionComponent
+import React from "react";
+import { cn, getSubjectColor } from "@/lib/utils";
+import { useState } from "react";
+
+enum CallStatus {
+    INACTIVE = 'INACTIVE',
+    CONNECTING = 'CONNECTING',
+    ACTIVE = 'ACTIVE',
+    FINISHED = 'FINISHED',
+}
+const CompanionComponent = ({
+  companionId,
+  subject,
+  topic,
+  name,
+  userName,
+  userImage,
+  style,
+  voice,
+}: CompanionComponentProps) => {
+    const [callStatus, setCallStatus] = useState<CallStatus>(CallStatus.INACTIVE);  return (
+    <section className="flex flex-col h-[70vh]">
+      <section className="flex gap-8 max-sm:flex-col">
+        <div className="companion-section">
+          <div
+            className="companion-avatar"
+            style={{ backgroundColor: getSubjectColor(subject) }}
+          >
+            <div
+              className={cn("absolute transition-opacity duration-1000")}
+            ></div>
+          </div>
+        </div>
+      </section>
+    </section>
+  );
+};
+
+export default CompanionComponent;
